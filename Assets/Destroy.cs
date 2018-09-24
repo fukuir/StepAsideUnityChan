151e340c0022eaf08f01534a37e053716aefb78d
FukuiRyoutarou <calicocatff37@icloud.com>
2018-09-24 17:08:52 +0900

    StepAsideUnityChan
    画面外に出たアイテムを削除する課題用

---
+++ Assets/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
